944162579f8edb82596f33174bf3ab18e8dcf9bb
zaheerbrakchan <xaheer3scc@gmail.com>
2025-10-16 16:42:57 +0530

    Fix Railway Python runtime detection

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-set -e  # Exit if any command fails
+set -e
 
-# Go to backend folder
 cd backend
-
-# Ensure pip and dependencies are installed
-python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-# Start the FastAPI app
 python3 -m uvicorn main:app --host 0.0.0.0 --port 8000
